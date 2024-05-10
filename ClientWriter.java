5f34afdb4bfe9f1e1cec358519696b04ab7840a1
Lorenzo Bandini <lboa@hotmail.it>
2024-05-10 15:11:03 +0200

    Refactor ClientWriter to use Scanner for user input and PrintWriter for server communication

--- ClientWriter.java
+++ ClientWriter.java
@@ -1,15 +1,37 @@
+import java.io.PrintWriter;
+import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientWriter implements Runnable{
 
     private Socket clientSocket;
+    private PrintWriter out;
+    private Scanner scanner;
 
     public ClientWriter(Socket clientSocket) {
         this.clientSocket = clientSocket;
+        try {
+            this.out = new PrintWriter(clientSocket.getOutputStream(), true);
+            this.scanner = new Scanner(System.in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run() {
-        
+        // Scrivi al server in continuazione
+        String message;
+        while (!(message = scanner.nextLine()).equals("exit")) {
+            out.println(message);
+        }
+        out.close();
+        scanner.close();
+        try {
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
