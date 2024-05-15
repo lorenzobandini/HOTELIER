79d58106acfe077ffe7dbca69ad30e92d3724864
Lorenzo Bandini <lboa@hotmail.it>
2024-05-15 23:39:13 +0200

    add atomic boolean for check the closure of socket

--- src/ClientWriter.java
+++ src/ClientWriter.java
@@ -2,14 +2,16 @@ import java.io.PrintWriter;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ClientWriter implements Runnable {
 
     private Socket clientSocket;
     private PrintWriter out;
     private Scanner scanner;
+    private AtomicBoolean isConnected;
 
-    public ClientWriter(Socket clientSocket) {
+    public ClientWriter(Socket clientSocket, AtomicBoolean isConnected) {
         this.clientSocket = clientSocket;
         try {
             this.out = new PrintWriter(clientSocket.getOutputStream(), true);
@@ -17,6 +19,7 @@ public class ClientWriter implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        this.isConnected = isConnected;
     }
 
     @Override
@@ -29,6 +32,7 @@ public class ClientWriter implements Runnable {
         out.println("exit");
         out.close();
         scanner.close();
+        isConnected.set(false);
         try {
             clientSocket.close();
         } catch (IOException e) {
