7f36c1ffb080f6715bb502a27ac3335c0e5d45d5
Lorenzo Bandini <lboa@hotmail.it>
2024-05-16 16:48:13 +0200

    formatting

--- src/main/java/com/unipi/lorenzobandini/hotelier/model/Review.java
+++ src/main/java/com/unipi/lorenzobandini/hotelier/model/Review.java
@@ -2,32 +2,31 @@ package com.unipi.lorenzobandini.hotelier.model;
 
 import java.time.LocalDate;
 
-public class Review{
+public class Review {
     private String reviewer;
     private LocalDate date = LocalDate.now();
     private float globalScore;
     private Ratings scores;
 
-    public Review(String reviewer, float globalScore, Ratings scores){
+    public Review(String reviewer, float globalScore, Ratings scores) {
         this.reviewer = reviewer;
-        this.globalScore = Math.round(globalScore*10)/10.0f;
+        this.globalScore = Math.round(globalScore * 10) / 10.0f;
         this.scores = scores;
     }
 
-    public String getReviewer(){
+    public String getReviewer() {
         return this.reviewer;
     }
 
-    public float getGlobalScore(){
+    public float getGlobalScore() {
         return this.globalScore;
     }
 
-
-    public LocalDate getDate(){
+    public LocalDate getDate() {
         return this.date;
     }
 
-    public Ratings getRatings(){
+    public Ratings getRatings() {
         return this.scores;
     }
 
