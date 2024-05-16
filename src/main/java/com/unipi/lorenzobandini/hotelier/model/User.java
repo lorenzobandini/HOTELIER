3e4a14971a0d8a8fbfacb2168bcb9605238428b7
Lorenzo Bandini <lboa@hotmail.it>
2024-05-16 16:48:02 +0200

    formatting

--- src/main/java/com/unipi/lorenzobandini/hotelier/model/User.java
+++ src/main/java/com/unipi/lorenzobandini/hotelier/model/User.java
@@ -1,13 +1,13 @@
 package com.unipi.lorenzobandini.hotelier.model;
 
-public class User{
+public class User {
     private String username;
     private String hashPassword;
     private boolean isLogged;
     private String badge;
     private int reviewCount;
 
-    public User(String username, String hashPassword, boolean isLogged, String badge, int reviewCount){
+    public User(String username, String hashPassword, boolean isLogged, String badge, int reviewCount) {
         this.username = username;
         this.hashPassword = hashPassword;
         this.isLogged = isLogged;
@@ -15,47 +15,47 @@ public class User{
         this.reviewCount = reviewCount;
     }
 
-    public String getUsername(){
+    public String getUsername() {
         return this.username;
     }
 
-    public String getHashPassword(){
+    public String getHashPassword() {
         return this.hashPassword;
     }
 
-    public boolean isLogged(){
+    public boolean isLogged() {
         return this.isLogged;
     }
 
-    public void setLogged(boolean isLogged){
+    public void setLogged(boolean isLogged) {
         this.isLogged = isLogged;
     }
 
-    public String getBadge(){
+    public String getBadge() {
         return this.badge;
     }
 
-    public void setBadge(String badge){
+    public void setBadge(String badge) {
         this.badge = badge;
     }
 
-    public int getReviewCount(){
+    public int getReviewCount() {
         return this.reviewCount;
     }
 
-    public void addReview(){
+    public void addReview() {
         this.reviewCount++;
         setBadge();
     }
 
-    private void setBadge(){
-        if(this.reviewCount >= 5 && this.reviewCount < 10){
+    private void setBadge() {
+        if (this.reviewCount >= 5 && this.reviewCount < 10) {
             this.badge = "Recensore Esperto";
-        }else if(this.reviewCount >= 10 && this.reviewCount < 20){
+        } else if (this.reviewCount >= 10 && this.reviewCount < 20) {
             this.badge = "Contributore";
-        }else if(this.reviewCount >= 20 && this.reviewCount < 30){
+        } else if (this.reviewCount >= 20 && this.reviewCount < 30) {
             this.badge = "Contributore Esperto";
-        }else if(this.reviewCount >= 30){
+        } else if (this.reviewCount >= 30) {
             this.badge = "Contributore Super";
         }
     }
